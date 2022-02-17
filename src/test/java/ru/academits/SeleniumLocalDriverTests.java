c7001a15330fa82ef811ab4029743d22f458ff2c
sfk999 <avm-expert@ya.ru>
2022-02-17 17:48:29 +0700

    .1

--- src/test/java/ru/academits/SeleniumLocalDriverTests.java
+++ src/test/java/ru/academits/SeleniumLocalDriverTests.java
@@ -3,11 +3,24 @@ package ru.academits;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Calendar;
+
 public class SeleniumLocalDriverTests {
 
     public void openPageChromeTest() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\a.muzhanovskiy\\Desktop chromedriver.exe ");
-        WebDriver webDriver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+
+
+
+
+
+
+    }
+
+    public void getYear() {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        System.out.println(year);
     }
 }
