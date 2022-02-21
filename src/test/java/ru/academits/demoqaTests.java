f995d1722c4f4475379b9ee574d8fd6f89d3f652
sfk999 <avm-expert@ya.ru>
2022-02-21 16:54:47 +0700

    21.02

--- src/test/java/ru/academits/demoqaTests.java
+++ src/test/java/ru/academits/demoqaTests.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class demoqaTests {
@@ -35,66 +36,94 @@ public class demoqaTests {
     }
 
     @Test
-    public void inputFirstNameTest() throws InterruptedException {
+    public void Tests() throws InterruptedException {
 
-        WebElement inputFirstName = driver.findElement(By.id("firstName"));
-        inputFirstName.sendKeys("John");
-        Assertions.assertEquals("John" , inputFirstName.getAttribute("value"));
+        WebElement firstNameInput = driver.findElement(By.id("firstName"));
+        firstNameInput.sendKeys("John");
+        Assertions.assertEquals("John" , firstNameInput.getAttribute("value"));
 
-        WebElement inputLastName = driver.findElement(By.id("lastName"));
-        inputLastName.sendKeys("Smith");
-        Assertions.assertEquals("Smith" , inputLastName.getAttribute("value"));
+        WebElement lastNameInput = driver.findElement(By.id("lastName"));
+        lastNameInput.sendKeys("Smith");
+        Assertions.assertEquals("Smith" , lastNameInput.getAttribute("value"));
 
-        WebElement inputEmail = driver.findElement(By.id("userEmail"));
-        inputEmail.sendKeys("input@test.com");
-        Assertions.assertEquals("input@test.com" , inputEmail.getAttribute("value"));
+        WebElement emailInput = driver.findElement(By.id("userEmail"));
+        emailInput.sendKeys("input@test.com");
+        Assertions.assertEquals("input@test.com" , emailInput.getAttribute("value"));
 
         WebElement radioButton = driver.findElement(By.cssSelector("#genterWrapper"));
         radioButton.click();
         Assertions.assertFalse(false , radioButton.getAttribute("checked"));
 
-        WebElement inputUserNumber = driver.findElement(By.id("userNumber"));
-        inputUserNumber.sendKeys("8800555353");
-        Assertions.assertEquals("8800555353" , inputUserNumber.getAttribute("value"));
+        WebElement userNumberInput = driver.findElement(By.id("userNumber"));
+        userNumberInput.sendKeys("8800555353");
+        Assertions.assertEquals("8800555353" , userNumberInput.getAttribute("value"));
 
         WebElement dateOfBirth = driver.findElement(By.id("dateOfBirthInput"));
         dateOfBirth.click();
-        WebElement year = driver.findElement(By.xpath("//*[text()='1944']"));
-        year.click();
-        WebElement month = driver.findElement(By.xpath("//*[text()='August']"));
-        month.click();
-        WebElement day = driver.findElement(By.xpath("//*[text()='28']"));
-        day.click();
+        WebElement yearOfBirth = driver.findElement(By.xpath("//*[text()='1944']"));
+        yearOfBirth.click();
+        WebElement monthOfBirth = driver.findElement(By.xpath("//*[text()='August']"));
+        monthOfBirth.click();
+        WebElement dayOfBirth = driver.findElement(By.xpath("//*[text()='28']"));
+        dayOfBirth.click();
         Assertions.assertEquals("28 Aug 1944" , dateOfBirth.getAttribute("value"));
 
 
-//        WebElement inputSubjects = driver.findElement(By.id("subjectsContainer"));
-//        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", inputSubjects);
-//        inputSubjects.click();
-//        inputSubjects.sendKeys("Subject");
-//        inputSubjects.sendKeys(Keys.ENTER);
-//        скрипт , ошибка element not interactable
+        WebElement subjectsInput = driver.findElement(By.id("subjectsInput"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", subjectsInput);
+        subjectsInput.click();
+        subjectsInput.sendKeys("h");
+        subjectsInput.sendKeys(Keys.ENTER);
+
 
         WebElement checkbox = driver.findElement(By.xpath("//*[text()='Sports']"));
         checkbox.click();
         Assertions.assertTrue(true , checkbox.getAttribute("checked"));
 
-//        WebElement uploadPicture = driver.findElement(By.id("uploadPicture"));
-//        uploadPicture.click();
-//        uploadPicture.sendKeys("ru/academits/Снимок экрана 2022-02-20 191113.jpg");
-//        uploadPicture
+        WebElement uploadPicture = driver.findElement(By.id("uploadPicture"));
+        uploadPicture.sendKeys("C:/Users/a.muzhanovskiy/IdeaProjects/selenium-tests1/src/test/java/ru/academits/Снимок экрана 2022-02-20 191113.jpg");
+
+
+//        File file = new File("src/test/java/ru/academits/Снимок экрана 2022-02-20 191113.jpg");
+//        System.out.println("path is " + file.getAbsolutePath());
+//       C:\Users\a.muzhanovskiy\IdeaProjects\selenium-tests1\src\test\java\ru\academits\Снимок экрана 2022-02-20 191113.jpg
+
 
         WebElement currentAddress = driver.findElement(By.id("currentAddress"));
         currentAddress.sendKeys("Москва, Тверская, 8");
         Assertions.assertEquals("Москва, Тверская, 8" , currentAddress.getAttribute("value"));
 
-//        WebElement selectState = driver.findElement(By.id("state"));
-//        selectState.click();
-//        driver.findElement(By.xpath("//*[text()='Uttar Pradesh']")).click();
+        WebElement stateDropdown = driver.findElement(By.id("state"));
+        stateDropdown.click();
+        driver.switchTo().activeElement().sendKeys("Uttar Pradesh");
+        driver.switchTo().activeElement().sendKeys(Keys.ENTER);
 
-//        WebElement selectCity = driver.findElement(By.id("city"));
-//        selectCity.click();
-//        driver.findElement(By.xpath("//*[text()='Agra']")).click();
+        WebElement cityDropdown = driver.findElement(By.id("city"));
+        cityDropdown.click();
+        driver.switchTo().activeElement().sendKeys("Merrut");
+        driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+
+
+        WebElement EmailAssert = driver.findElement(By.cssSelector("tbody>tr:nth-child(2)>td:nth-child(2)"));
+        System.out.println("text " + EmailAssert.getAttribute("value"));
+        Assertions.assertEquals("input@test.com" , EmailAssert.getAttribute("value"));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         Thread.sleep(5000);
